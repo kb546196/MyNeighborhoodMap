f9704c369ac9fa8ff64ba8a800b3beae275321cb
kb546196 <KB@Kates-MBP.home>
2016-12-23 19:02:45 +0000

    Song lyrics appear in info windows
    Changed order of title to alphabetical. Changed Ajax request and info
    window content

--- js/app.js
+++ js/app.js
@@ -78,12 +78,52 @@ var highlightedIcon = 'images/yellow_record.png';
 var SongLyricsID = [];
 //Array of song/ location objects  
 var tracklist = [{
+    title: "Baker Street by Gerry Rafferty",
+    trackId: 17488873,
+    address: "Baker Street",
+    location: {
+        lat: 51.520610,
+        lng: -0.15685
+    }
+},{
+    title: "Common People by Pulp",
+    trackId: 31069277,
+    address: "St Martins College",
+    location: {
+        lat: 51.522691,
+        lng: -0.108875
+    }
+}, {
+    title: "Electric Avenue by Eddy Grant",
+    trackId: 14581872,
+    address: "Brixton Market",
+    location: {
+        lat: 51.462168,
+        lng: -0.114004
+    }
+},{
+    title: "Fake Plastic Trees by Radiohead",
+    trackId: 30715806,
+    address: "Canary Wharf",
+    location: {
+        lat: 51.505431,
+        lng: -0.023533
+    }
+},{ 
     title: "Fool on the Hill by The Beatles",
     trackId: 15109547,
     address: "Primrose Hill",
     location: {
         lat: 51.538449,
         lng: -0.154993
+    }
+}, {
+    title: "I Don't Want To Go To Chelsea by Elvis Costello",
+    trackId: 94003508,
+    address: "Chelsea, London",
+    location: {
+        lat: 51.485093,
+        lng: -0.174936
     }
 }, {
     title: "Itchycoo Park by Small Faces",
@@ -94,12 +134,12 @@ var tracklist = [{
         lng: 0.066476
     }
 }, {
-    title: "Waterloo Sunset by The Kinks",
-    trackId: 30056157,
-    address: "Waterloo Bridge",
+    title: "River Lea by Adele",
+    trackId: 84213315,
+    address: "River Lea",
     location: {
-        lat: 51.501737,
-        lng: -0.108588
+        lat: 51.595541,
+        lng: -0.052013
     }
 }, {
     title: "The Battle of Epping Forest by Genesis",
@@ -108,6 +148,47 @@ var tracklist = [{
     location: {
         lat: 51.657077,
         lng: 0.041282
+    }
+},  {
+    title: "The Guns of Brixton by The Clash",
+    trackId: 31180665,
+    address: "Brixton",
+    location: {
+        lat: 51.453349,
+        lng: -0.120576
+    }
+},      {
+    title: "Up The Bracket by The Libertines",
+    trackId: 1198194,
+    address: "Bethnal Green",
+    location: {
+        lat: 51.526974,
+        lng: -0.06672
+    } 
+
+},  {
+    title: "Up the Junction by Squeeze",
+    trackId: 96456433,
+    address: "Clapham",
+    location: {
+        lat: 51.465174,
+        lng: -0.170811
+    }
+},{
+    title: "Upper Clapton Dance by Professor Green",
+    trackId: 5765357,
+    address: "Upper Clapton",
+    location: {
+        lat: 51.567230,
+        lng: -0.06119
+    }
+}, {
+    title: "Waterloo Sunset by The Kinks",
+    trackId: 30056157,
+    address: "Waterloo Bridge",
+    location: {
+        lat: 51.501737,
+        lng: -0.108588
     }
 }, {
     title: "Werewolves of London by Warren Zevon",
@@ -118,54 +199,6 @@ var tracklist = [{
         lng: -0.131086
     }
 }, {
-    title: "Hong Kong Garden by Siouxsie and the Banshees",
-    trackId: 95550325,
-    address: "Chislehurst",
-    location: {
-        lat: 51.412626,
-        lng: 0.077487
-    }
-}, {
-    title: "I Don't Want To Go To Chelsea by Elvis Costello",
-    trackId: 17561092,
-    address: "Chelsea, London",
-    location: {
-        lat: 51.485093,
-        lng: -0.174936
-    }
-}, {
-    title: "Baker Street by Gerry Rafferty",
-    trackId: 17561092,
-    address: "Baker Street",
-    location: {
-        lat: 51.520610,
-        lng: -0.15685
-    }
-}, {
-    title: "Up the Junction by Squeeze",
-    trackId: 17489271,
-    address: "Clapham",
-    location: {
-        lat: 51.465174,
-        lng: -0.170811
-    }
-}, {
-    title: "The Guns of Brixton by The Clash",
-    trackId: 31180665,
-    address: "Brixton",
-    location: {
-        lat: 51.453349,
-        lng: -0.120576
-    }
-}, {
-    title: "Electric Avenue by Eddy Grant",
-    trackId: 14581872,
-    address: "Brixton Market",
-    location: {
-        lat: 51.462168,
-        lng: -0.114004
-    }
-}, {
     title: "West End Girls by Pet Shop Boys",
     trackId: 30936055,
     address: "Soho",
@@ -173,47 +206,10 @@ var tracklist = [{
         lat: 51.513614,
         lng: -0.136549
     }
-}, {
-    title: "Common People by Pulp",
-    trackId: 31069277,
-    address: "St Martins College",
-    location: {
-        lat: 51.522691,
-        lng: -0.108875
-    }
-}, {
-    title: "Fake Plastic Trees by Radiohead",
-    trackId: 30715806,
-    address: "Canary Wharf",
-    location: {
-        lat: 51.505431,
-        lng: -0.023533
-    }
-}, {
-    title: "Up The Bracket by The Libertines",
-    trackId: 1198194,
-    address: "Bethnal Green",
-    location: {
-        lat: 51.526974,
-        lng: -0.06672
-    }
-}, {
-    title: "Upper Clapton Dance by Professor Green",
-    trackId: 5765357,
-    address: "Upper Clapton",
-    location: {
-        lat: 51.567230,
-        lng: -0.06119
-    }
-}, {
-    title: "River Lea by Adele",
-    trackId: 84213315,
-    address: "River Lea",
-    location: {
-        lat: 51.595541,
-        lng: -0.052013
-    }
 }];
+
+
+
 //Function to show the GoogleMap on the site (centred nr Barbican central London)
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -237,15 +233,14 @@ function initMap() {
         //KO observable array for song lyrics to go into 
         var SongLyricsArray = ko.observableArray(); 
 
-        for (var j = 0; j < tracklist.length; j++) {
+        
+        function getLyrics(musixmatchID, SongTitle) { 
 
-             var trackId = tracklist[j].trackId;
-            //Ajax request lyrics using track_id from Musixmatch 
-             $.ajax({
+            $.ajax({
                 type: "GET",
                 data: {
                     apikey: "445d6196c08dc2b7490929f18149d684",
-                    track_id: trackId,
+                    track_id: musixmatchID,
                     format: "jsonp",
                 },
                 url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
@@ -256,33 +251,42 @@ function initMap() {
                     var LyricsBody = data.message.body.lyrics.lyrics_body;
                     //checking it has arrived 
                     //console.log(LyricsBody);
-                    SongLyricsArray.push(LyricsBody);   
-                    
+                    SongLyricsArray.push(SongTitle + " lyrics from Musixmatch." + LyricsBody); 
+
+                    SongLyricsArray.sort();   
 
                     //attempting to add to each info window (div should have the ID of the track ID)
                     //$(divID).append('<p>' + LyricsBody + 'Lyrics from Musixmatch</p>');
                 },
+
                 error: function() {
-                    console.log("Error w/ Ajax request");
-                    SongLyricsArray.push("Lyrics not currently loading - try again later");
+                    SongLyricsArray.push(SongTitle + " lyrics are not currently loading from Musixmatch - please try again later");
+
+                    SongLyricsArray.sort();  
                 }
             });
-
-         } 
+        
+    } 
 
         //for loop to go through each tracklist object creating marker position full title and position
         for (var i = 0; i < tracklist.length; i++) {
             var position = tracklist[i].location;
             var title = tracklist[i].title;
             var address = tracklist[i].address;
-           
-            SongLyricsID.push(trackId);
+            var songNo = i; 
+             var trackId = tracklist[i].trackId;
+            //Ajax request lyrics using track_id from Musixmatch 
+             
+
+            getLyrics(trackId, title); 
+            
             //Use variables fomr above to create property for each song marker
             var marker = new google.maps.Marker({
                 map: map,
                 position: position,
                 address: address,
                 title: title,
+                songNo: songNo,
                 icon: defaultIcon,
                 animation: google.maps.Animation.DROP
             });
@@ -303,16 +307,18 @@ function initMap() {
             self.showMarker = function(clickedItem) {
                     self.populateInfoWindow(clickedItem.marker, largeInfowindow)
                 }
+
+
                 //links info window and describes info to put in their
             self.populateInfoWindow = function(marker, infowindow) {
                 if (infowindow.marker != marker) {
                     infowindow.marker = marker;
+
                     infowindow.setContent(
                         '<div><h4>' + marker.title + '</h4>' +
-                        '<p> A song inspired by ' + marker.address + '</p>' +
-                        '<div>' + SongLyricsArray()[i] + '</div>' +
-                        '<p>Find out more about <a href="https://en.wikipedia.org/wiki/' +
-                        marker.address + '">' + marker.address + '</a>, on Wikipedia.</p>');
+                        '<p> A song inspired by ' + marker.address + ', find out more about <a href="https://en.wikipedia.org/wiki/' +
+                        marker.address + '">' + marker.address + '</a>, on Wikipedia.</p>' +
+                        '<p>' + SongLyricsArray()[marker.songNo] + '</p>');
                     infowindow.open(map, marker);
                     //listern to close marker
                     infowindow.addListener('closeclick', function() {
